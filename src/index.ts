ebb9c35bb9ed5b000be122ad51c3dcbe142fbd23
HeitorPolastri <polastrit@gmail.com>
2025-09-27 18:54:54 -0300

    feat: inicializador

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { app } from "./app";
 import { postRouter } from "./routes/postRouter";
 import { userRouter } from "./routes/userRouter";
 
+
 app.use("/users", userRouter);
 app.use("/posts", postRouter);

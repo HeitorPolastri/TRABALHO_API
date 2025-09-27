459f87b11c1d4035b4de44d22f60d31999c117c9
HeitorPolastri <polastrit@gmail.com>
2025-09-27 19:00:55 -0300

    feat: user data add

--- src/data/PostData.ts
+++ src/data/PostData.ts
@@ -1,12 +1,11 @@
 import { posts, Post } from "../bd";
 
 export class PostData {
-  // Retorna todos os posts (necessário para UserBusiness validar inativos)
+  
   getAllPosts = (): Post[] => {
     return posts;
   };
 
-  // Cria um novo post (EXERCÍCIO 3 - parte data)
   createPost = (title: string, content: string, authorId: number): Post => {
     const newPost: Post = {
       id: posts.length + 1,
@@ -20,12 +19,10 @@ export class PostData {
     return newPost;
   };
 
-  // Busca um post por ID para modificação ou visualização
   getPostById = (postId: number): Post | undefined => {
     return posts.find((p) => p.id === postId);
   };
 
-  // Deleta um post
   deletePost = (postId: number): boolean => {
     const postIndex = posts.findIndex((p) => p.id === postId);
     if (postIndex === -1) {
